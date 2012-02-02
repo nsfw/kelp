39a6b8fed0cf9f5255a84cc3c6f8071b425a0e25
Scott Watson <scott@disney.com>
2012-02-02 09:49:46 -0800

    Full 8x8 mapping - uses pins along top of chipKIT

--- GE35mapping.h
+++ GE35mapping.h
@@ -28,9 +28,11 @@
 
 // CONFIGURATION INFORMATION 
 #define MAX_STRAND_LEN 35	// Should be the ACTUAL LENGTH OF LONGEST STRAND - electrical max is 62
-#define STRAND_COUNT 2		// Needs to be ACTUAL NUMBER OF DATA LINES IN USE
-#define IMG_WIDTH 4			
-#define IMG_HEIGHT 8		// 
+#define STRAND_COUNT (8*4)		// Needs to be ACTUAL NUMBER OF DATA LINES IN USE
+
+// storage for 8x8x8 - mapped as an 8 x 64 tall image
+#define IMG_WIDTH (8)			
+#define IMG_HEIGHT (8*8)		// 
 
 typedef struct a_strand {
     byte len;		// length of this strand
@@ -39,14 +41,55 @@ typedef struct a_strand {
     byte y[MAX_STRAND_LEN];
 } strand;
 
+// !!NOTE!! PANEL MACRO assumes that PINS are CONTIGUOUS and INCREASEd
+
+// Need 32 pins 
 // J3 (chipKIT) 78-85 -- upper left dual HDI
 #define PIN_PANEL_0_STRAND_A 78
 #define PIN_PANEL_0_STRAND_B 79
 #define PIN_PANEL_0_STRAND_C 80
 #define PIN_PANEL_0_STRAND_D 81
 
+#define PIN_PANEL_1_STRAND_A 82
+#define PIN_PANEL_1_STRAND_B 83
+#define PIN_PANEL_1_STRAND_C 84
+#define PIN_PANEL_1_STRAND_D 85
+
+#define PIN_PANEL_2_STRAND_A 8
+#define PIN_PANEL_2_STRAND_B 9
+#define PIN_PANEL_2_STRAND_C 10
+#define PIN_PANEL_2_STRAND_D 11
+
+#define PIN_PANEL_3_STRAND_A 78
+#define PIN_PANEL_3_STRAND_B 79
+#define PIN_PANEL_3_STRAND_C 80
+#define PIN_PANEL_3_STRAND_D 81
+
+// J14 upper middle dual HDI
+#define PIN_PANEL_4_STRAND_A 70
+#define PIN_PANEL_4_STRAND_B 71
+#define PIN_PANEL_4_STRAND_C 72
+#define PIN_PANEL_4_STRAND_D 73
+
+#define PIN_PANEL_5_STRAND_A 2
+#define PIN_PANEL_5_STRAND_B 3
+#define PIN_PANEL_5_STRAND_C 4
+#define PIN_PANEL_5_STRAND_D 5
+
+// 
+#define PIN_PANEL_6_STRAND_A 14
+#define PIN_PANEL_6_STRAND_B 15
+#define PIN_PANEL_6_STRAND_C 16
+#define PIN_PANEL_6_STRAND_D 17
+
+#define PIN_PANEL_7_STRAND_A 18
+#define PIN_PANEL_7_STRAND_B 19
+#define PIN_PANEL_7_STRAND_C 20
+#define PIN_PANEL_7_STRAND_D 21
 
 #ifndef GE35_NO_DATA
+
+// MACROS to condense mapping 
 
 #define UP_DOWN_X(a,b) \
     {\
@@ -60,32 +103,42 @@ typedef struct a_strand {
      }
 
 
-#define UP_DOWN_Y \
+#define UP_DOWN_Y(y) \
     {\
-          0,1,2,3,4,5,6,7,                                \
-                        7,	/* down */ \
-          7,6,5,4,3,2,1,0, \
-          0,    /* spare */ \
-          0,1,2,3,4,5,6,7, \
-                        7,	/* down */\
-          7,6,5,4,3,2,1,0 \
+          0+y,1+y,2+y,3+y,4+y,5+y,6+y,7+y,                                \
+			                          7+y,	/* down */ \
+          7+y,6+y,5+y,4+y,3+y,2+y,1+y,0+y, \
+          0+y,    /* spare */ \
+          0+y,1+y,2+y,3+y,4+y,5+y,6+y,7+y, \
+			                          7+y,	/* down */\
+          7+y,6+y,5+y,4+y,3+y,2+y,1+y,0+y \
     }
 
-#define STRANDS(PIN,X) \
+#define STRANDS(PIN,X,BASE_Y) \
     { /*len*/ 35,	\
       /*pin*/ PIN,  \
       UP_DOWN_X(X,X+1), \
-      UP_DOWN_Y	\
+      UP_DOWN_Y(BASE_Y) \
     }
 
+// !!NOTE!! Macro assumes that PINS are CONTIGUOUS and INCREASEd
+#define PANEL(PIN,Z) \
+    STRANDS(PIN+0,0,Z*8),	\
+    STRANDS(PIN+1,2,Z*8),	\
+    STRANDS(PIN+2,4,Z*8),	\
+    STRANDS(PIN+3,6,Z*8)
 
+// Each panel layer is mapped in the Y direction
 strand strands[]={
 // len, pin, {x-coords}{y-coords}, initial color
-    /* panel 0 */
-    STRANDS(PIN_PANEL_0_STRAND_A,0),
-    STRANDS(PIN_PANEL_0_STRAND_B,2),
-    STRANDS(PIN_PANEL_0_STRAND_C,4),
-    STRANDS(PIN_PANEL_0_STRAND_D,6),
+    PANEL(PIN_PANEL_0_STRAND_A, 0),
+    PANEL(PIN_PANEL_1_STRAND_A, 1),
+    PANEL(PIN_PANEL_2_STRAND_A, 2),
+    PANEL(PIN_PANEL_3_STRAND_A, 3),
+    PANEL(PIN_PANEL_4_STRAND_A, 4),
+    PANEL(PIN_PANEL_5_STRAND_A, 5),
+    PANEL(PIN_PANEL_6_STRAND_A, 6),
+    PANEL(PIN_PANEL_7_STRAND_A, 7)
 };
 
 #endif	// GE35_NO_DATA
