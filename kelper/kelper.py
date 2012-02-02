a87ae9415058d5f83795f2991ff8a64df1ca6ccf
Scott Watson <scott@disney.com>
2012-02-02 10:38:13 -0800

    pixels now sent as rgba and destination x,y is sent as part of image message

--- kelper/kelper.py
+++ kelper/kelper.py
@@ -11,13 +11,15 @@ def getPixel(mov,frameOffset,x,y,z,options):
     # index into the source movie (which is a one dimensional array)
     # organized as RED[0...63], GRN[0...63], BLU[0...63]
     # support ROTATIONS which map the given x,y,z -> x',y',z'
+    # NOTE: turns r,g,b pixels into r,g,b,a pixels
     nx=x
     ny=y
     nz=z
     pixOff = (z*8*8) + (y*8) + x
     return [mov[frameOffset+pixOff],
             mov[frameOffset+pixOff+64],
-            mov[frameOffset+pixOff+128]]
+            mov[frameOffset+pixOff+128],
+            255]		# alpha or something else?
 
 
 def composeFrame(mov, frameNum, base, options):
@@ -33,15 +35,23 @@ def composeFrame(mov, frameNum, base, options):
 
 def sendFrame(kelp, frame):
     # this is an awful hack to send a blob w/o doing CCoreOSC surgery
-    # Further, max Ethernet size is ~1500 bytes, so we need send the frame
+    # Further(!), max Ethernet size is ~1500 bytes, so we need send the frame
     # as two packets...
     m = CCore.OSC.OSCMessage()
-    m.setAddress("/rgb888/top")
+    m.setAddress("/screenxy");
+    m.append(8)	# image size
+    m.append(8)
+    m.append(0)	# x=0
+    m.append(0) # y=top half of image
     m.append(frame[0:64*3*4],typehint='b')		# needs blob typehint
     kelp.sender.send(m)
     time.sleep(0.001)
     # send second half
-    m.clear("/rgb888/bot")
+    m.clear("/screenxy");
+    m.append(8)		# image size
+    m.append(8)
+    m.append(0)		# x=0
+    m.append(8*4)	# y=bottom half of image
     m.append(frame[64*3*4:],typehint='b')		# needs blob typehint
     kelp.sender.send(m)
 
@@ -51,6 +61,7 @@ def sendFrame(kelp, frame):
 #  Compose Frame N (w/ transforms)
 #  Send Frame N
 #  Delay
+
 
 filenm = "../media/raw888/TestXYZ_8x8x8_color.raw"
 f = open(filenm, "r")
